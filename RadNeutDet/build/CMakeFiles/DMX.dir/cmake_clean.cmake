66098f347f3a00ff0b2074d0922f492c400589b9
asantra <santra.arka@gmail.com>
2025-08-04 15:31:18 +0530

    merging Neutron bkg

---
+++ RadNeutDet/build/CMakeFiles/DMX.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DMX.dir/DMX.cc.o"
+  "CMakeFiles/DMX.dir/DMX.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXActionInitialization.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXActionInitialization.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXDetectorConstruction.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXDetectorConstruction.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXEventAction.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXEventAction.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXMaxTimeCuts.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXMaxTimeCuts.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXMinEkineCuts.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXMinEkineCuts.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXPhysicsList.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXPhysicsList.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXPrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXRunAction.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXRunAction.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXSpecialCuts.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXSpecialCuts.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXStackingAction.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXStackingAction.cc.o.d"
+  "CMakeFiles/DMX.dir/src/DMXSteppingAction.cc.o"
+  "CMakeFiles/DMX.dir/src/DMXSteppingAction.cc.o.d"
+  "DMX"
+  "DMX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DMX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
